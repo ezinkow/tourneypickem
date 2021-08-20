3c9ad4cb4d67f3e2e074ff689dedf3ef7d6e8666
Elan Zinkow <ezinkow@gmail.com>
2021-08-19 23:44:45 -0500

    m

--- client/src/pages/Testimonials.js
+++ client/src/pages/Testimonials.js
@@ -3,14 +3,14 @@ import Navbar from '../components/Navbar'
 import TestimonialsCards from '../components/TestimonialsCards'
 import TestimonialsCarousel from '../components/TestimonialsCarousel'
 import TestimonialsForm from '../components/TestimonialsForm'
-import TestHeader from '../images/testimonialsheader.png'
+import TestimonialsHeader from '../images/testimonialsHeader.png'
 
 export default function () {
 
     return (
         <>
             <Navbar />
-            <img src={TestHeader} alt='testimonials' className="container header"/>
+            <img src={TestimonialsHeader} alt='testimonials' className="container header"/>
             <TestimonialsCarousel />
             <TestimonialsCards />
             <TestimonialsForm />
