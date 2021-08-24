ff67e3b95e70f490e917326e81499ce783a29395
Elan Zinkow <ezinkow@gmail.com>
2021-08-24 09:46:38 -0500

    adding images

--- client/src/pages/Ask.js
+++ client/src/pages/Ask.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import AskQuestionForm from '../components/AskQuestionForm'
 import AskHeader from '../images/askheader.png'
+import dad3 from '../images/dad3.jpg'
+import dad6 from '../images/dad6.jpg'
 import Navbar from '../components/Navbar.js'
+
 
 
 export default function Ask() {
@@ -10,7 +13,19 @@ export default function Ask() {
         <>
             <Navbar />
             <img src={AskHeader} alt='Ask!' className='header' />
-            <AskQuestionForm />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-4'>
+                        <img src={dad3} alt='dad3' className='askImg'/>
+                    </div>
+                    <div className='col-4'>
+                        <AskQuestionForm />
+                    </div>
+                    <div className='col-4'>
+                        <img src={dad6} alt='dad6' className='askImg'/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- client/src/pages/Testimonials.js
+++ client/src/pages/Testimonials.js
@@ -4,14 +4,28 @@ import TestimonialsCards from '../components/TestimonialsCards'
 import TestimonialsCarousel from '../components/TestimonialsCarousel'
 import TestimonialsForm from '../components/TestimonialsForm'
 import TestimonialsHeader from '../images/testimonialsheader.png'
+import dad4 from '../images/dad4.jpg'
+import dad5 from '../images/dad5.jpg'
 
 export default function () {
 
     return (
         <>
             <Navbar />
-            <img src={TestimonialsHeader} alt='testimonials' className="header"/>
-            <TestimonialsCarousel />
+            <img src={TestimonialsHeader} alt='testimonials' className="header" />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-4'>
+                        <img src={dad4} alt='dad4' className='askImg' />
+                    </div>
+                    <div className='col-5'>
+                        <TestimonialsCarousel />
+                    </div>
+                    <div className='col-3'>
+                        <img src={dad5} alt='dad5' className='askImg' />
+                    </div>
+                </div>
+            </div>
             <TestimonialsCards />
             <TestimonialsForm />
         </>
