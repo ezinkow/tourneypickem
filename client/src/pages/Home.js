3ad83404a71dff76499641cadb7faaa2a229345d
Elan Zinkow <ezinkow@gmail.com>
2023-02-10 11:56:17 -0600

    m

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Comments from '../components/Comments'
-import Birkat from '../components/Birkat'
 
 
 export default function Home() {
@@ -10,7 +8,6 @@ export default function Home() {
     return (
         <div>
             <Navbar />
-            <Comments />
         </div>
     )
 }
