b0a086a7e2d0f1c5132dd158beb6c2a6af25dae5
Elan Zinkow <ezinkow@gmail.com>
2023-02-16 10:14:44 -0600

    navbar fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,40 +8,34 @@ import {
 export default function Navbar() {
 
     return (
-        <>
-            <div className="header">
-                <div className="navbar">
-                    <div className='container'>
-                        <div className="col-2 logoImg">
-                            <img src={logo} alt='logo' className='logoImg' />
-                        </div>
-                        <div className="col-2">
-                        </div>
-                        <div className="col-5 navtext">
-                            <Link to='/'><p>Home</p></Link>
-                        </div>
-                        <div className="col-5 navtext">
-                            <Link to='/birkat'><p>Birkat Hamazon</p></Link>
-                        </div>
-                        <div className="col-2">
-                        </div>
-                        <div className="col-5 navtext">
-                            <Link to='/comment'><p>Leave a Comment!</p></Link>
-                        </div>
-                        <div className="col-5 navtext">
-                            <Link to='/comments'><p>Comments</p></Link>
-                        </div>
-                    </div>
-                    <br />
-                    <div className="col-2">
-                    </div>
-                    <div className="col-8">
-                        <p className='mazel'>MAZEL TOV AMIR AND DANA!!!</p>
+        <div className="header">
+            <div className="navbar">
+                <div className='container'>
+                    <div className="col-2 logoImg">
+                        <img src={logo} alt='logo' className='logoImg' />
                     </div>
                     <div className="col-2">
+                    </div>
+                    <div className="col-5 navtext">
+                        <Link to='/'><p>Home</p></Link>
+                    </div>
+                    <div className="col-5 navtext">
+                        <Link to='/birkat'><p>Birkat Hamazon</p></Link>
+                    </div>
+                    <div className="col-2">
+                    </div>
+                    <div className="col-5 navtext">
+                        <Link to='/comment'><p>Leave a Comment!</p></Link>
+                    </div>
+                    <div className="col-5 navtext">
+                        <Link to='/comments'><p>Comments</p></Link>
                     </div>
                 </div>
+                <br />
+                <div className="col-12">
+                    <p className='mazel'>MAZEL TOV AMIR AND DANA!!!</p>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
