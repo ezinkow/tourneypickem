7bd975822d4e68b224ae917076a19ef816b7e7cf
Elan Zinkow <ezinkow@gmail.com>
2023-03-11 17:06:41 -0600

    311 pm

--- client/src/components/PicksDisplay.js
+++ client/src/components/PicksDisplay.js
@@ -5,7 +5,7 @@ import Table from 'react-bootstrap/Table';
 
 export default function PicksDisplay() {
     const [picks, setPicks] = useState([])
-    const todaysDate = '311a'
+    const todaysDate = '311p'
 
     useEffect(() => {
         async function fetchPicks() {
