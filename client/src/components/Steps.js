ac6db79d6b0b6500d8cdb70c7e62a857f5102efb
Elan Zinkow <ezinkow@gmail.com>
2023-03-14 22:06:32 -0500

    steps

--- client/src/components/Steps.js
+++ client/src/components/Steps.js
@@ -13,6 +13,7 @@ export default function Steps() {
                 <li>Picks will show up in the table down below</li>
                 <li>When ready, submit your picks. If you need to make a change, just reach out to me. Don't re-submit picks</li>
                 <li>The google doc will still house your picks and results</li>
+                <li>If the line is a whole number, the underdog gets a half point (ex. -3 moves to -3.5)</li>
             </ul>
         </>
     )
