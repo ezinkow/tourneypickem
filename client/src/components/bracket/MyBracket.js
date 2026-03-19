a2acc3fd18c2caccec8229efe0c5723da9ad8c23
Elan Zinkow <ezinkow@gmail.com>
2026-03-19 09:58:18 -0500

    update bracket lock time

--- client/src/components/bracket/MyBracket.js
+++ client/src/components/bracket/MyBracket.js
@@ -6,8 +6,8 @@ import BracketRegion from "../../components/bracket/BracketRegion";
 
 const GOLD = "#c89d3c";
 const BLUE = "#13447a";
-const LOCK_TIME = new Date("2026-03-19T11:18:00-05:00");
-const CHAMP_TIPOFF = new Date("2026-04-06T21:20:00-05:00");
+const LOCK_TIME = new Date("2026-03-19T12:18:00-05:00");
+const CHAMP_TIPOFF = new Date("2026-04-06T21:20:00-05:00"); 
 const ROUND_LABELS = {
     1: "1st Round", 2: "2nd Round", 3: "Sweet 16",
     4: "Elite 8", 5: "Final Four", 6: "Championship"
